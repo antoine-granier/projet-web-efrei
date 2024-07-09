2e9a0b82b04a26556c09d0a80ed26666ca967c02
antoine-granier <78824041+antoine-granier@users.noreply.github.com>
2024-07-09 14:14:28 +0200

    connect front to back

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,8 +5,65 @@ import {
 } from "react-router-dom";
 import LayoutPage from "./Pages/LayoutPage";
 import Login from "./Pages/Login";
+import {
+  ApolloClient,
+  ApolloLink,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+import { useEffect, useRef } from "react";
+import { jwtDecode } from "jwt-decode";
+import { useUserStore } from "./store/userStore";
+import Register from "./Pages/Register";
 
 function App() {
+  const { user, setUser } = useUserStore();
+
+  const httpLink = new HttpLink({ uri: import.meta.env.VITE_API_URL });
+
+  const authLink = new ApolloLink((operation, forward) => {
+    operation.setContext({
+      headers: {
+        authorization: user?.token ? `Bearer ${user.token}` : "",
+      },
+    });
+
+    return forward(operation);
+  });
+
+  const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+  });
+
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
+  const maxTimeoutDelayBrowser = 2147483647;
+  const token = user?.token;
+
+  useEffect(() => {
+    if (token) {
+      const decoded = jwtDecode(token);
+      const time = new Date().getTime();
+
+      if (decoded.exp) {
+        const expirationTime = decoded.exp * 1000 - time;
+        timeoutRef.current = setTimeout(
+          () => {
+            setUser(null);
+          },
+          expirationTime > maxTimeoutDelayBrowser
+            ? maxTimeoutDelayBrowser
+            : expirationTime
+        );
+      }
+    }
+
+    return () => {
+      if (timeoutRef.current) clearTimeout(timeoutRef.current);
+    };
+  }, [user, setUser, user?.token]);
+
   const modules = [
     {
       path: "home",
@@ -22,15 +79,11 @@ function App() {
     },
     {
       path: "/register",
-      element: <div>Register</div>,
+      element: <Register />,
     },
     {
       path: "/",
-      element: true ? (
-        <LayoutPage modules={modules} />
-      ) : (
-        <Navigate to="/login" />
-      ),
+      element: user?.token ? <LayoutPage /> : <Navigate to="/login" />,
       children: [
         {
           path: "",
@@ -41,7 +94,11 @@ function App() {
     },
   ]);
 
-  return <RouterProvider router={router} />;
+  return (
+    <ApolloProvider client={client}>
+      <RouterProvider router={router} />
+    </ApolloProvider>
+  );
 }
 
 export default App;
--- frontend/src/Pages/LayoutPage.tsx
+++ frontend/src/Pages/LayoutPage.tsx
@@ -1,14 +1,11 @@
 import { Button } from "flowbite-react";
 import { IoIosAdd, IoMdExit } from "react-icons/io";
-import { Outlet, RouteObject } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { twMerge } from "tailwind-merge";
+import { useUserStore } from "../store/userStore";
 
-type LayoutPageProps = {
-  modules: RouteObject[];
-};
-
-const LayoutPage: React.FC<LayoutPageProps> = ({ modules }) => {
-  console.log(modules);
+const LayoutPage: React.FC = () => {
+  const user = useUserStore((state) => state.user);
 
   return (
     <div
@@ -19,7 +16,7 @@ const LayoutPage: React.FC<LayoutPageProps> = ({ modules }) => {
     >
       <div className="w-1/3 border-r flex flex-col gap-2">
         <div className="flex justify-between items-center px-2 py-4 border-b">
-          <p>Dany Derensy</p>
+          <p>{user?.name}</p>
           <div className="flex items-center gap-2">
             <Button size="xs">
               <IoIosAdd className="h-6 w-6" />
